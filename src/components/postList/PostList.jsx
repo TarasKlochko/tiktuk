d8796e82a47c9c030d1ee3ba76d32545147c1adf
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2021-11-16 12:44:37 +0200

    feat: add pagination

--- src/components/postList/PostList.jsx
+++ src/components/postList/PostList.jsx
@@ -3,9 +3,13 @@ import './PostList.css';
 
 export default function PostList() {
   const [posts, setPosts] = useState();
+  const [firstPost, setFirstPost] = useState(0);
+  const [lastPost, setLastPost] = useState(10);
+  const postPerPage = 10;
+  const postsFromApi = 21;
 
   useEffect(() => {
-    fetch('https://tiktok33.p.rapidapi.com/trending/feed?&limit=10', {
+    fetch(`https://tiktok33.p.rapidapi.com/trending/feed?&limit=${postsFromApi}`, {
       method: 'GET',
       headers: {
         'x-rapidapi-host': 'tiktok33.p.rapidapi.com',
@@ -22,37 +26,75 @@ export default function PostList() {
       });
   }, []);
 
+  function handlePrevPosts() {
+    if (firstPost > 0) {
+      setFirstPost(firstPost - postPerPage);
+      setLastPost(lastPost - postPerPage);
+    }
+  }
+
+  function handleNextPosts() {
+    if (lastPost < postsFromApi) {
+      setFirstPost(firstPost + postPerPage);
+      setLastPost(lastPost + postPerPage);
+    }
+  }
+
   return (
-    <ul className="post-list">
-      {posts &&
-        posts.map((post) => (
-          <li className="post-list__item" key={post.id}>
-            <iframe
-              className="post-list__video"
-              src={post.videoUrl}
-              title="YouTube video player"
-              frameBorder="0"
-              sandbox=""
-              allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            />
-            <div className="post-list__info">
-              <div className="post-list__avatar">
-                <img className="post-list__avatar-img" src={post.authorMeta.avatar} alt="avatar" />
-              </div>
-              <div className="post-list__info-wrap">
-                <p className="post-list__info-text">{post.text}</p>
-                <h2 className="post-list__info-name"> {post.authorMeta.name}</h2>
-                {post.hashtags && (
-                  <p className="post-list__info-hash">{post.hashtags.map((hash) => `#${hash.name} `)}</p>
-                )}
-                <div className="post-list__info-coment-like-wrap">
-                  <p className="post-list__info-coment">{post.commentCount ? post.commentCount : '0'} coments</p>{' '}
-                  <p className="post-list__info-likes">{post.diggCount ? post.diggCount : '0'} likes</p>
-                </div>
-              </div>
-            </div>
-          </li>
-        ))}
-    </ul>
+    <>
+      <ul className="post-list">
+        {posts &&
+          posts.map((post, index) => {
+            if (index >= firstPost && index < lastPost) {
+              return (
+                <li className="post-list__item" key={post.id}>
+                  <iframe
+                    className="post-list__video"
+                    src={post.videoUrl}
+                    title="YouTube video player"
+                    frameBorder="0"
+                    sandbox=""
+                    allowFullScreen
+                    allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  />
+                  <div className="post-list__info">
+                    <div className="post-list__avatar">
+                      <img className="post-list__avatar-img" src={post.authorMeta.avatar} alt="avatar" />
+                    </div>
+                    <div className="post-list__info-wrap">
+                      <p className="post-list__info-text">{post.text}</p>
+                      <h2 className="post-list__info-name"> {post.authorMeta.name}</h2>
+                      {post.hashtags && (
+                        <p className="post-list__info-hash">{post.hashtags.map((hash) => `#${hash.name} `)}</p>
+                      )}
+                      <div className="post-list__info-coment-like-wrap">
+                        <p className="post-list__info-coment">{post.commentCount ? post.commentCount : '0'} coments</p>{' '}
+                        <p className="post-list__info-likes">{post.diggCount ? post.diggCount : '0'} likes</p>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              );
+            }
+            return null;
+          })}
+      </ul>
+      <div className="pagination-wrap">
+        <button
+          type="button"
+          className={firstPost ? 'prev-posts' : ' prev-posts prev-posts_disabled'}
+          onClick={handlePrevPosts}
+        >
+          Prev
+        </button>
+        <button
+          type="button"
+          className={lastPost < postsFromApi ? 'prev-posts' : 'prev-posts prev-posts_disabled'}
+          onClick={handleNextPosts}
+        >
+          Next
+        </button>
+      </div>
+    </>
   );
 }
