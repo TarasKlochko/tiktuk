bac1790079bff5d725ea8f5469f493e121bdb2e9
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2021-11-17 13:47:11 +0200

    refactor: improve code

--- src/components/postList/PostList.jsx
+++ src/components/postList/PostList.jsx
@@ -59,64 +59,68 @@ export default function PostList() {
   return (
     <>
       {isLoading && <Loading />}
-      <ul className="post-list">
-        {posts &&
-          !errorFromAPI &&
-          posts.map((post, index) => {
-            if (index >= firstPost && index < lastPost) {
-              return (
-                <li className="post-list__item" key={post.id}>
-                  <iframe
-                    className="post-list__video"
-                    src={post.videoUrl}
-                    title="YouTube video player"
-                    frameBorder="0"
-                    sandbox=""
-                    allowFullScreen
-                    allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  />
-                  <div className="post-list__info">
-                    <div className="post-list__avatar" onClick={() => handleUser(post.authorMeta.name)}>
-                      <img className="post-list__avatar-img" src={post.authorMeta.avatar} alt="avatar" />
-                    </div>
-                    <div className="post-list__info-wrap">
-                      <p className="post-list__info-text">{post.text}</p>
-                      <h2 className="post-list__info-name" onClick={() => handleUser(post.authorMeta.name)}>
-                        {post.authorMeta.name}
-                      </h2>
-                      {post.hashtags && (
-                        <p className="post-list__info-hash">{post.hashtags.map((hash) => `#${hash.name} `)}</p>
-                      )}
-                      <div className="post-list__info-coment-like-wrap">
-                        <p className="post-list__info-coment">{post.commentCount ? post.commentCount : '0'} coments</p>{' '}
-                        <p className="post-list__info-likes">{post.diggCount ? post.diggCount : '0'} likes</p>
+
+      {posts && !errorFromAPI && (
+        <>
+          <ul className="post-list">
+            {posts.map((post, index) => {
+              if (index >= firstPost && index < lastPost) {
+                return (
+                  <li className="post-list__item" key={post.id}>
+                    <iframe
+                      className="post-list__video"
+                      src={post.videoUrl}
+                      title="YouTube video player"
+                      frameBorder="0"
+                      sandbox=""
+                      allowFullScreen
+                      allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    />
+                    <div className="post-list__info">
+                      <div className="post-list__avatar" onClick={() => handleUser(post.authorMeta.name)}>
+                        <img className="post-list__avatar-img" src={post.authorMeta.avatar} alt="avatar" />
+                      </div>
+                      <div className="post-list__info-wrap">
+                        <p className="post-list__info-text">{post.text}</p>
+                        <h2 className="post-list__info-name" onClick={() => handleUser(post.authorMeta.name)}>
+                          {post.authorMeta.name}
+                        </h2>
+                        {post.hashtags && (
+                          <p className="post-list__info-hash">{post.hashtags.map((hash) => `#${hash.name} `)}</p>
+                        )}
+                        <div className="post-list__info-coment-like-wrap">
+                          <p className="post-list__info-coment">
+                            {post.commentCount ? post.commentCount : '0'} coments
+                          </p>
+                          <p className="post-list__info-likes">{post.diggCount ? post.diggCount : '0'} likes</p>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </li>
-              );
-            }
-            return null;
-          })}
-      </ul>
-      {posts && (
-        <div className="pagination-wrap">
-          <button
-            type="button"
-            className={firstPost ? 'prev-posts' : ' prev-posts prev-posts_disabled'}
-            onClick={handlePrevPosts}
-          >
-            Prev
-          </button>
-          <button
-            type="button"
-            className={lastPost < postsFromApi ? 'prev-posts' : 'prev-posts prev-posts_disabled'}
-            onClick={handleNextPosts}
-          >
-            Next
-          </button>
-        </div>
+                  </li>
+                );
+              }
+              return null;
+            })}
+          </ul>
+          <div className="pagination-wrap">
+            <button
+              type="button"
+              className={firstPost ? 'prev-posts' : ' prev-posts prev-posts_disabled'}
+              onClick={handlePrevPosts}
+            >
+              Prev
+            </button>
+            <button
+              type="button"
+              className={lastPost < postsFromApi ? 'prev-posts' : 'prev-posts prev-posts_disabled'}
+              onClick={handleNextPosts}
+            >
+              Next
+            </button>
+          </div>
+        </>
       )}
+
       {errorFromAPI && <ErrorMessage errorMessage={errorFromAPI} />}
     </>
   );
